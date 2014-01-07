1f08dce1146d9def184c89652d2fbea5180169da
Aarti Munjal <aarti.munjal@ucdenver.edu>
2014-01-06 17:37:59 -0700

    added two php mailer scripts for sending studydesign and power results emails, respectively. Still need to update the attachments and test sending emails.

---
+++ app/powerResults_email.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>PHPMailer - Power Results Email</title>
+</head>
+<body>
+<?php
+require_once('lib/PHPMailer/class.phpmailer.php');
+require_once('lib/PHPMailer/class.smtp.php');
+
+try {
+    //Create a new PHPMailer instance
+    $mail = new PHPMailer();
+    // Set PHPMailer to use the sendmail transport
+    $mail->isSMTP();
+
+    //Set who the message is to be sent from
+    $mail->setFrom('samplesizeshop@gmail.com', 'Sample Size Shop');
+    //$mail->addReplyTo($_REQUEST["email"], $_REQUEST["name"]);
+
+    //Set who the message is to be sent to
+      $mail->addAddress($_REQUEST["email"], $_REQUEST["name"]);
+
+    //Set the subject line
+    $mail->Subject = 'Power Results File';
+
+    // create the message body
+    $mail->Body = "<html><head></head><body>";
+    $mail->Body .= "<p>Dear " . $_REQUEST["name"] . "</p>";
+    $mail->Body .= "<p>Details:</p><p>" . 'Thank you for using Glimmpse.
+    Your power results are attached to this email. To learn more about
+    power and sample size, please visit http://samplesizeshop.org.' . "</p>";
+    $mail->Body .= "</body></html>";
+
+           $mail->AltBody = "Please use an HTML compatible email viewer";
+
+    //Attach a file
+    if (isset($_FILES["file"])) {
+        $mail->addAttachment($_FILES["file"]["tmp_name"], $_FILES["file"]["name"]);
+    }
+
+    //send the message, check for errors
+    if (!$mail->send()) {
+        echo $mail->ErrorInfo;
+        http_response_code(503);
+    } else {
+        echo "OK";
+    }
+
+} catch (phpmailerException $e) {
+    echo $e->errorMessage(); //error messages from PHPMailer
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
+?>
+</body>
+</html>
