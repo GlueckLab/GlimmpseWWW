6ad347f355fdedbb361096190effec4bfde056dc
Wes Munsil <wes_munsil@cytoanalytics.com>
2015-06-22 11:02:47 -0600

    SSS-87: set C.maxRows to B.rows unconditionally; add checks on rows and columns to isValidMatrix.

--- app/js/controllers/controllers.js
+++ app/js/controllers/controllers.js
@@ -3463,12 +3463,9 @@ glimmpseApp.controller('stateController',
             $scope.betweenContrastMatrix =
                 studyDesignService.getMatrixByName(glimmpseConstants.matrixBetweenContrast);
             $scope.maxRows = 1;
-            var designEssence = studyDesignService.getMatrixByName(glimmpseConstants.matrixXEssence);
-            if (designEssence !== undefined) {
-                var dim = Math.min(designEssence.rows, designEssence.columns);
-                if (dim > 1) {
-                    $scope.maxRows = designEssence.columns;
-                }
+            var beta = studyDesignService.getMatrixByName(glimmpseConstants.matrixBeta);
+            if (beta !== undefined) {
+                $scope.maxRows = beta.rows;
             }
         }
 
